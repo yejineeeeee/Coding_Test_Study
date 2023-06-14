828661e0fcce3cc174861d37aa9eedafcfe4df04
yejineeeeee <dpwls0454@naver.com>
2023-06-15 02:00:41 +0900

    정수 삼각형

--- 프로그래머스/level 3/정수 삼각형.py
+++ 프로그래머스/level 3/정수 삼각형.py
@@ -1,11 +1,12 @@
 def solution(triangle):
-    for i in range(1, len(triangle)): 
-        for j in range(i+1):
-            if j == 0:
+
+    for i in range(1, len(triangle)): # i = 몇번째 줄인지
+        for j in range(i+1): # j = 줄 안에서 인덱스
+            if j == 0: # 가장 왼쪽인 경우
                 triangle[i][j] += triangle[i-1][j]
-            elif j == i:
+            elif j == i: # 가장 오른쪽인 경우
                 triangle[i][j] += triangle[i-1][-1]
-            else:
-                triangle[i][j] += max ( triangle[i-1][j-1], triangle[i-1][j])
-    
+            else: # 가운데인 경우
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+                
     return max(triangle[-1])
