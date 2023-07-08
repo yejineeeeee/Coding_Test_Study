831cdd6e96571edc6bfe4fa8177f9fe5ba69243f
yejin <dpwls0454@naver.com>
2023-07-09 00:44:25 +0900

    개인정보 수집 유효기간

---
+++ 프로그래머스/level 1/개인정보 수집 유효기간.py
@@ -0,0 +1,23 @@
+def dateToDay(date):
+    year, month, day = map(int, date.split("."))
+    return (year*12*28) + (month*28) + day
+
+def solution(today, terms, privacies):
+    answer = []
+
+    #today
+    today = dateToDay(today)
+
+    #term
+    termsInfo = dict()
+    for term in terms:
+        term = term.split()
+        termsInfo[term[0]] = int(term[1])*28
+    
+    #privacies
+    for i in range(len(privacies)):
+        date, term = privacies.split()
+        if dateToDay(date) + termsInfo[term] <= today:
+            answer.append(i+1)
+
+    return answer
